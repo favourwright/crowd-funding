71f5a1a3280a5c44ae6260c5f5b24c34e6391fdb
Favour <gismozfexie@yahoo.com>
2023-09-07 19:05:14 +0100

    chore: minor update to fix deployment

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,15 @@
  
  const App = () => {
    return (
-     <div>App</div>
+    <div className="relative sm:-8 p-4 bg-[#13131a] text-white min-h-screen flex flex-row">
+      <div className="sm:flex hidden mr-10 relative">
+        Sidebar
+      </div>
+
+      <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5">
+        Navbar
+      </div>
+    </div>
    )
  }
  
