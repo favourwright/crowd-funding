fe1e3f8dbf05ce93ad4c565f296fd8e709f0b99e
Favour <gismozfexie@yahoo.com>
2023-09-08 00:20:57 +0100

    feat: add layout components and pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,32 @@
- import React from 'react'
+import React from 'react'
+import { Routes, Route } from 'react-router-dom' 
+import {
+  Home,
+  Profile,
+  CreateCampaign,
+  CampaignDetails,
+} from './pages'
+import { Sidebar, Navbar } from './components'
  
- const App = () => {
-   return (
+const App = () => {
+  return (
     <div className="relative sm:-8 p-4 bg-[#13131a] text-white min-h-screen flex flex-row">
       <div className="sm:flex hidden mr-10 relative">
-        Sidebar
+        <Sidebar />
       </div>
 
       <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5">
-        Navbar
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<Home />} /> 
+          <Route path="/profile" element={<Profile />} /> 
+          <Route path="/create" element={<CreateCampaign />} /> 
+          <Route path="/details" element={<CampaignDetails />} /> 
+        </Routes>
       </div>
     </div>
-   )
- }
+  )
+}
  
  export default App
